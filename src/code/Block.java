9de7de91c4a2649ff2b3ccf9ec5417656e8f0228
HiromuHiramatsu <s14t256@stmail.eng.kagawa-u.ac.jp>
2016-11-08 14:46:30 +0900

    Block

---
+++ src/code/Block.java
@@ -0,0 +1,41 @@
+package code;
+
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Block extends GameObject {
+	int block[] = new int[30];
+	int weight[] = new int[30];
+	int height[] = new int[30]; 
+	static Color color = Color.PINK;
+	static int NUM = 30;
+	
+	public Block(int x, int y) {
+		super(x, y);
+	}
+	
+	public void init() {
+		int k;
+		int i = 0;
+		
+		for ( k = 0; k < NUM; k++ ) {
+			if ( k % 10 == 0 ) {
+				
+			}
+		}
+	}
+	
+	public void draw(Graphics g) {
+		create(g);
+	}
+	
+	public void create(Graphics g) {
+		int i; // カウント
+		for ( i = 0; i < NUM; i++ ) {
+			g.setColor(color);
+			g.fillRect(weight[i], height[i], weight[i]+x, height[i]+y);
+		}
+		
+	}
+	
+}
