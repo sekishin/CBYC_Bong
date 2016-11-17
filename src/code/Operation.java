be1ac76490cb0cbad281b32df87e8f6cb25a48a1
HiromuHiramatsu <s14t256@stmail.eng.kagawa-u.ac.jp>
2016-11-17 15:02:37 +0900

    push

--- src/code/Operation.java
+++ src/code/Operation.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
+import javax.swing.JLabel;
+
 public class Operation {
 	
 	private int x, y;
@@ -12,19 +14,20 @@ public class Operation {
 	public Operation (int x, int y, int w, int h) {
 		this.x = x;
 		this.y = y;
-		this.width = w;
-		this.height = h;
+		this.width = 30;
+		this.height = 30;
 	}
 	
 	public void draw(Graphics g) {
-		java.awt.Font font = new Font("ＭＳ 明朝", 20, 20);
+		java.awt.Font font1 = new Font("ＭＳ 明朝", 20, 20);
+		java.awt.Font font2 = new Font("ＭＳ 明朝", 30, 30);
 		int a = x-150;
 		int b = y;
 		int c = x;
 		int d = y;
 		
 		g.setColor(Color.BLACK);
-		g.setFont(font);
+		g.setFont(font1);
 		g.drawString("赤", x-405, y+15);
 		g.drawString("R", x-400, y+40);
 		g.drawString("F", x-400, y+60);
@@ -37,25 +40,22 @@ public class Operation {
 		g.drawString("剛球", x-370, y+60);
 		g.drawString("巨大化", x-370, y+80);
 		
-		g.drawString("↑", a, b+20);
-		g.drawString("←", a-30, b+55);
-		g.drawString("→", a+30, b+55);
-		g.drawString("↓", a, b+90);
-		
-		g.drawString("A", a+5, b+40);       // 上の操作
+		g.setFont(font2);
+		drawButton(g, 0, 0, "Hello");
+		g.drawString("W", a+5, b+40);       // 上の操作
 		g.drawString("A", a-10, b+55);  // 左の操作
-		g.drawString("A", a+20, b+55); // 右の操作
-		g.drawString("A", a+5, b+70);   // 下の操作
+		g.drawString("S", a+20, b+55); // 右の操作
+		g.drawString("Z", a+5, b+70);   // 下の操作
 		
-		g.drawString("↑", c, d+20);
-		g.drawString("←", c-30, d+55);
-		g.drawString("→", c+30, d+55);
-		g.drawString("↓", c, d+90);
-		
-		g.drawString("B", c+5, d+40);        // 上の操作
-		g.drawString("B", c-10, d+55);  // 左の操作
-		g.drawString("B", c+20, d+55);   // 右の操作
-		g.drawString("B", c+5, d+70);    // 下の操作
+		g.drawString("I", c+5, d+40);        // 上の操作
+		g.drawString("J", c-10, d+55);  // 左の操作
+		g.drawString("K", c+20, d+55);   // 右の操作
+		g.drawString("M", c+5, d+70);    // 下の操作
+	}
+	
+	public void drawButton ( Graphics g, int x, int y, String str ) {
+		g.drawString(str, x, y);
+		g.fill3DRect(x, y, width, height, true);
 	}
 
 }
