40e57c47b352b37ccf5d586780e9e6ffce383957
shivashin <s14t241@stmail.eng.kagawa-u.ac.jp>
2016-11-20 15:54:40 +0900

    add something.

--- src/code/Field.java
+++ src/code/Field.java
@@ -11,14 +11,14 @@ import javax.swing.ImageIcon;
 
 
 public class Field extends GameObject implements ImageObserver{
-	
+
 	private Image image;
-	private String path = "../image/homo1.jpg";
+	private String path = "image/homo1.jpg";
 	private GameSound ah = new GameSound("music/Ah.wav");
-	private boolean flag = false; 
+	private boolean flag = false;
 	private static Color InitialColor = Color.BLUE;
 	private Random rand = new Random();
-	
+
 	public Field (int x, int y, int w, int h) {
 		super(x, y, w, h, InitialColor);
 		switch (rand.nextInt(4)+1) {
@@ -30,35 +30,35 @@ public class Field extends GameObject implements ImageObserver{
 		loadImage(path);
 		this.rect = new Rectangle(this.x, this.y, this.width, this.height);
 	}
-	
+
 	public void showImage() {
 		if (this.flag) return;
 		this.flag = true;
 		ah.play();
 	}
-	
+
 	@Override
 	public Type getType() {
 		return Type.Field;
 	}
-	
+
 	@Override
 	public void draw (Graphics g) {
 		g.setColor(color);
 		g.fillRect(this.x, this.y, this.width, this.height);
 		if (flag) g.drawImage(image, this.x, this.y, this.width, this.height, this);
 	}
-	
-	
+
+
 	public void loadImage (String path) {
 		ImageIcon icon = new ImageIcon(getClass().getResource(path));
-		image = icon.getImage();		
+		image = icon.getImage();
 	}
 
 	@Override
 	public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
 		return false;
 	}
-	
-	
+
+
 }
